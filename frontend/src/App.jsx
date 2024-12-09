62a5d30baa3d9f9300fcd53cc02bea9202271856
Mouwneam <124057919+Mouwneam@users.noreply.github.com>
2024-12-09 20:34:40 +0700

    add basename to createBrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
     path: "menu/products/:productId",
     element: <ProductDetail></ProductDetail>
   }
-]);
+], {basename: "/Cafe-Shop/frontend"});
   return(
     <main className='relative'>
 
